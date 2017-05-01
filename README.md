e33915eb016405367eb6cb38e9c778f68cf67700
zqbright <zhengqiguang1@yy.com>
2017-05-01 17:07:47 +0800

    微信小程序手势库 1.0

---
+++ README.md
@@ -0,0 +1,87 @@
+##WxTouchEvent 微信小程序手势事件库
+
+由于微信小程序只能够支持 tap,longtap,touchstart,touchmove,touchcancel,touchend时间，对于比较复杂的事件只能自己实现
+因此自己对 `alloyFinger`库进行了改造，开发了时候微信小程序手势事件库`WxTouchEvent`,使用 ES
+6进行编写,手势库支持以下事件
+
+*   touchStart
+*   touchMove
+*   touchEnd
+*   touchCancel
+*   multipointStart
+*   multipointEnd
+*   tap
+*   doubleTap
+*   longTap
+*   singleTap
+*   rotate
+*   pinch
+*   pressMove
+*   swipe
+
+
+###使用
+由于和微信小程序强绑定，因此需要在元素上面绑定好所有的事件，书写比较麻烦，因此建议对于原生支持的使用原生去解决，
+只有当需要 pinch，rotate,swipe 等特殊事件才使用这个事件库实现
+
+> 安装 `npm i wx-touch-event --save`
+
+绑定方法
+#### \*.wxml
+在`wxml`中对需要监听时间的元素绑定 `touchstart、touchmove、touchend、touchcancel`四个事件
+页面书写成
+
+``` 
+    <view class="info-list" 
+          bindtouchstart="touchStart"
+          bindtouchmove="touchMove"
+          bindtouchend="touchEnd"
+          bindtouchcancel="touchCancel"
+        >
+        
+    </view>
+```
+
+#### \*.js
+在`js`逻辑层需要实例化`WxTouchEvent`, 实例中有`start、move、end、cancel`对应`\*.wxml`绑定的`bindtouchstart,bindtouchmove,bindtouchend,bindtouchcancel`,需要将事件的回调函数一一对应,
+书写成:
+
+```
+import WxTouchEvent from "wx-touch-event";
+
+let infoListTouchEvent = new WxTouchEvent();//在 Page外实例化函数，可以直接复制给 Page 中的回调函数
+Page({
+    onLoad: function() {
+        this.infoListTouchEvent = infoListTouchEvent;
+        this.infoListTouchEvent.bind({//初始化后绑定事件
+            swipe: function(e) {
+                console.log(e);
+            },
+            doubleTap: function(e) {
+                console.log(e);
+            },
+            tap: function(e) {
+                console.log(e);
+            }.bind(this),
+            longTap: function(e) {
+                console.log(e);
+            },
+            rotate: function(e) {
+                console.log(e)
+            }.bind(this),
+            pinch: function(e) {
+                console.log(e);
+            }
+
+        })
+    },
+    touchStart: infoListTouchEvent.start.bind(infoListTouchEvent),
+    touchMove: infoListTouchEvent.move.bind(infoListTouchEvent),
+    touchEnd: infoListTouchEvent.end.bind(infoListTouchEvent),
+    touchCancel: infoListTouchEvent.cancel.bind(infoListTouchEvent),
+
+});
+```
+
+
+
---
+++ src/wx-touch-event.js
@@ -0,0 +1,296 @@
+/**
+ * Created by zhengqiguang on 2017/4/30.
+ */
+
+
+class WxTouchEvent {
+    constructor() {
+        this.preV = {x: null, y: null};
+        this.pinchStartLen = null;
+        this.scale = 1;
+        this.isDoubleTap = false;
+    }
+
+    bind(option) {
+        this.element = null;
+
+        this.rotate = wrapFunc(this.element, option.rotate || noop);
+        this.touchStart = wrapFunc(this.element, option.touchStart || noop);
+        this.multipointStart = wrapFunc(this.element, option.multipointStart || noop);
+        this.multipointEnd = wrapFunc(this.element, option.multipointEnd || noop);
+        this.pinch = wrapFunc(this.element, option.pinch || noop);
+        this.swipe = wrapFunc(this.element, option.swipe || noop);
+        this.tap = wrapFunc(this.element, option.tap || noop);
+        this.doubleTap = wrapFunc(this.element, option.doubleTap || noop);
+        this.longTap = wrapFunc(this.element, option.longTap || noop);
+        this.singleTap = wrapFunc(this.element, option.singleTap || noop);
+        this.pressMove = wrapFunc(this.element, option.pressMove || noop);
+        this.touchMove = wrapFunc(this.element, option.touchMove || noop);
+        this.touchEnd = wrapFunc(this.element, option.touchEnd || noop);
+        this.touchCancel = wrapFunc(this.element, option.touchCancel || noop);
+
+        this.delta = null;
+        this.last = null;
+        this.now = null;
+        this.tapTimeout = null;
+        this.singleTapTimeout = null;
+        this.longTapTimeout = null;
+        this.swipeTimeout = null;
+        this.x1 = this.x2 = this.y1 = this.y2 = null;
+        this.preTapPosition = {x: null, y: null};
+    }
+
+    start(evt) {
+        if (!evt.touches) return;
+        this.now = Date.now();
+        this.x1 = evt.touches[0].pageX;
+        this.y1 = evt.touches[0].pageY;
+        this.delta = this.now - (this.last || this.now);
+        this.touchStart.dispatch(evt);
+        if (this.preTapPosition.x !== null) {
+            this.isDoubleTap = (this.delta > 0 && this.delta <= 250 && Math.abs(this.preTapPosition.x - this.x1) < 30 && Math.abs(this.preTapPosition.y - this.y1) < 30);
+        }
+        this.preTapPosition.x = this.x1;
+        this.preTapPosition.y = this.y1;
+        this.last = this.now;
+        let preV = this.preV,
+            len = evt.touches.length;
+        if (len > 1) {
+            this._cancelLongTap();
+            this._cancelSingleTap();
+            let v = {x: evt.touches[1].pageX - this.x1, y: evt.touches[1].pageY - this.y1};
+            preV.x = v.x;
+            preV.y = v.y;
+            this.pinchStartLen = getLen(preV);
+            this.multipointStart.dispatch(evt);
+        }
+        this.longTapTimeout = setTimeout(function () {
+            evt.type = "longTap";
+            this.longTap.dispatch(evt);
+        }.bind(this), 750);
+    }
+
+    move(evt) {
+        if (!evt.touches) return;
+        let preV = this.preV,
+            len = evt.touches.length,
+            currentX = evt.touches[0].pageX,
+            currentY = evt.touches[0].pageY;
+        this.isDoubleTap = false;
+        if (len > 1) {
+            let v = {x: evt.touches[1].pageX - currentX, y: evt.touches[1].pageY - currentY};
+
+            if (preV.x !== null) {
+                if (this.pinchStartLen > 0) {
+                    evt.scale = getLen(v) / this.pinchStartLen;
+                    evt.type = "pinch";
+                    this.pinch.dispatch(evt);
+                }
+
+                evt.angle = getRotateAngle(v, preV);
+                evt.type = "rotate";
+                this.rotate.dispatch(evt);
+            }
+            preV.x = v.x;
+            preV.y = v.y;
+        } else {
+            if (this.x2 !== null) {
+                evt.deltaX = currentX - this.x2;
+                evt.deltaY = currentY - this.y2;
+
+            } else {
+                evt.deltaX = 0;
+                evt.deltaY = 0;
+            }
+            this.pressMove.dispatch(evt);
+        }
+
+        this.touchMove.dispatch(evt);
+
+        this._cancelLongTap();
+        this.x2 = currentX;
+        this.y2 = currentY;
+        if (len > 1) {
+            // evt.preventDefault();
+        }
+    }
+
+    end(evt) {
+        if (!evt.changedTouches) return;
+        this._cancelLongTap();
+        let self = this;
+        if (evt.touches.length < 2) {
+            this.multipointEnd.dispatch(evt);
+        }
+        this.touchEnd.dispatch(evt);
+        //swipe
+        if ((this.x2 && Math.abs(this.x1 - this.x2) > 30) ||
+            (this.y2 && Math.abs(this.y1 - this.y2) > 30)) {
+            evt.direction = this._swipeDirection(this.x1, this.x2, this.y1, this.y2);
+            this.swipeTimeout = setTimeout(function () {
+                evt.type = "swipe";
+                self.swipe.dispatch(evt);
+
+            }, 0)
+        } else {
+            this.tapTimeout = setTimeout(function () {
+                console.info("tap");
+                evt.type = "tap";
+                self.tap.dispatch(evt);
+                // trigger double tap immediately
+                if (self.isDoubleTap) {
+                    evt.type = "doubleTap";
+                    self.doubleTap.dispatch(evt);
+                    clearTimeout(self.singleTapTimeout);
+                    self.isDoubleTap = false;
+                }
+            }, 0)
+
+            if (!self.isDoubleTap) {
+                self.singleTapTimeout = setTimeout(function () {
+                    self.singleTap.dispatch(evt);
+                }, 250);
+            }
+        }
+
+        this.preV.x = 0;
+        this.preV.y = 0;
+        this.scale = 1;
+        this.pinchStartLen = null;
+        this.x1 = this.x2 = this.y1 = this.y2 = null;
+    }
+
+    cancel(evt) {
+        clearTimeout(this.singleTapTimeout);
+        clearTimeout(this.tapTimeout);
+        clearTimeout(this.longTapTimeout);
+        clearTimeout(this.swipeTimeout);
+        this.touchCancel.dispatch(evt);
+    }
+
+
+    _cancelLongTap() {
+        clearTimeout(this.longTapTimeout);
+    }
+
+    _cancelSingleTap() {
+        clearTimeout(this.singleTapTimeout);
+    }
+
+    _swipeDirection(x1, x2, y1, y2) {
+        return Math.abs(x1 - x2) >= Math.abs(y1 - y2) ? (x1 - x2 > 0 ? 'Left' : 'Right') : (y1 - y2 > 0 ? 'Up' : 'Down')
+    }
+
+    on(evt, handler) {
+        if (this[evt]) {
+            this[evt].add(handler);
+        }
+    }
+
+    off(evt, handler) {
+        if (this[evt]) {
+            this[evt].del(handler);
+        }
+    }
+
+    destroy() {
+        if (this.singleTapTimeout) clearTimeout(this.singleTapTimeout);
+        if (this.tapTimeout) clearTimeout(this.tapTimeout);
+        if (this.longTapTimeout) clearTimeout(this.longTapTimeout);
+        if (this.swipeTimeout) clearTimeout(this.swipeTimeout);
+
+        this.element.removeEventListener("touchstart", this.start);
+        this.element.removeEventListener("touchmove", this.move);
+        this.element.removeEventListener("touchend", this.end);
+        this.element.removeEventListener("touchcancel", this.cancel);
+
+        this.rotate.del();
+        this.touchStart.del();
+        this.multipointStart.del();
+        this.multipointEnd.del();
+        this.pinch.del();
+        this.swipe.del();
+        this.tap.del();
+        this.doubleTap.del();
+        this.longTap.del();
+        this.singleTap.del();
+        this.pressMove.del();
+        this.touchMove.del();
+        this.touchEnd.del();
+        this.touchCancel.del();
+
+        this.preV = this.pinchStartLen = this.scale = this.isDoubleTap = this.delta = this.last = this.now = this.tapTimeout = this.singleTapTimeout = this.longTapTimeout = this.swipeTimeout = this.x1 = this.x2 = this.y1 = this.y2 = this.preTapPosition = this.rotate = this.touchStart = this.multipointStart = this.multipointEnd = this.pinch = this.swipe = this.tap = this.doubleTap = this.longTap = this.singleTap = this.pressMove = this.touchMove = this.touchEnd = this.touchCancel = null;
+
+        return null;
+    }
+
+}
+
+let noop = function () {
+};
+
+function getLen(v) {
+    return Math.sqrt(v.x * v.x + v.y * v.y);
+}
+
+function dot(v1, v2) {
+    return v1.x * v2.x + v1.y * v2.y;
+}
+
+function getAngle(v1, v2) {
+    let mr = getLen(v1) * getLen(v2);
+    if (mr === 0) return 0;
+    let r = dot(v1, v2) / mr;
+    if (r > 1) r = 1;
+    return Math.acos(r);
+}
+
+function cross(v1, v2) {
+    return v1.x * v2.y - v2.x * v1.y;
+}
+
+function getRotateAngle(v1, v2) {
+    let angle = getAngle(v1, v2);
+    if (cross(v1, v2) > 0) {
+        angle *= -1;
+    }
+
+    return angle * 180 / Math.PI;
+}
+
+let HandlerAdmin = function (el) {
+    this.handlers = [];
+    this.el = el;
+};
+
+HandlerAdmin.prototype.add = function (handler) {
+    this.handlers.push(handler);
+}
+
+HandlerAdmin.prototype.del = function (handler) {
+    if (!handler) this.handlers = [];
+
+    for (let i = this.handlers.length; i >= 0; i--) {
+        if (this.handlers[i] === handler) {
+            this.handlers.splice(i, 1);
+        }
+    }
+}
+
+HandlerAdmin.prototype.dispatch = function () {
+    for (let i = 0, len = this.handlers.length; i < len; i++) {
+        let handler = this.handlers[i];
+        if (typeof handler === 'function') handler.apply(this.el, arguments);
+    }
+}
+
+function wrapFunc(el, handler) {
+    let handlerAdmin = new HandlerAdmin(el);
+    handlerAdmin.add(handler);
+
+    return handlerAdmin;
+}
+
+
+export default WxTouchEvent;
+
